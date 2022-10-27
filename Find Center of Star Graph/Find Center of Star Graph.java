c6fce9ca2cb0c894376a48e9e5b6dc09d270faae
Vivek Yadav <109095421+Viveky682@users.noreply.github.com>
2022-10-27 12:57:40 +0530

    Add files via upload

---
+++ Find Center of Star Graph/Find Center of Star Graph.java
@@ -0,0 +1,10 @@
+//1791. Find Center of Star Graph
+
+class Solution {
+    public int findCenter(int[][] edges) {
+        int x= edges[0][0];
+        int y= edges[0][1];
+        if(edges[1][0]==x || edges[1][1]==x) return x;
+        else return y;
+    }
+}
